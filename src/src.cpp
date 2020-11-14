3a0a2cfdb7fe863e81871e410bb00716a64a9c8d
takashi imagire <imagire@gmail.com>
2020-11-14 22:57:00 +0900

    git add src.cpp

--- src/src.cpp
+++ src/src.cpp
@@ -16,8 +16,14 @@
 #define HEIGHT 1080
 
 typedef struct {
-	unsigned char r, g, b;
-}cVector3;
+	float r, g, b;
+}Vector3;
+
+
+void RayTrace(float *dest, int width, int height)
+{
+
+}
 
 int main()
 {
@@ -25,25 +31,44 @@ int main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	// メモリリークチェック
 
-	cVector3 *image = (cVector3*)malloc(sizeof(cVector3) * WIDTH * HEIGHT);
+	// メモリイメージの作成
+	Vector3* image = (Vector3*)malloc(sizeof(Vector3) * WIDTH * HEIGHT);
 
 	for (int y = 0; y < HEIGHT; y++) {
+		Vector3* p = &image[y * WIDTH];
 		for (int x = 0; x < WIDTH; x++) {
-			cVector3* p = &image[y * WIDTH + x];
-			p->r = rand() & 0xff;
-			p->g = rand() & 0xff;
-			p->b = rand() & 0xff;
+			p->r = (float)rand() / (float)RAND_MAX;
+			p->g = (float)rand() / (float)RAND_MAX;
+			p->b = (float)rand() / (float)RAND_MAX;
+			p++;
 		}
 	}
 
-	// save
+
+	// 8bitのバッファに移す
+	unsigned char* file_image = (unsigned char*)malloc(3 * WIDTH * HEIGHT);
+	for (int y = 0; y < HEIGHT; y++) {
+		unsigned char* p = &file_image[3 * y * WIDTH];
+		const Vector3* s = &image[y * WIDTH];
+		for (int x = 0; x < WIDTH; x++) {
+			p[0] = (1.0f < s->r) ? 255 : (unsigned char)(255.9999f * s->r);
+			p[1] = (1.0f < s->g) ? 255 : (unsigned char)(255.9999f * s->g);
+			p[2] = (1.0f < s->b) ? 255 : (unsigned char)(255.9999f * s->b);
+			p += 3;
+			s++;
+		}
+	}
+
+
+	// PNG ファイルとして保存
 	int w = WIDTH;
 	int h = HEIGHT;
 	int comp = STBI_rgb; // RGB
-	int stride_in_bytes = 3 * w;
-	int result = stbi_write_png("result.png", w, h, comp, image, stride_in_bytes);
+	int stride_in_bytes = 3 * WIDTH;
+	int result = stbi_write_png("result.png", w, h, comp, file_image, stride_in_bytes);
 
 
+	free(file_image);
 	free(image);
 }
 
